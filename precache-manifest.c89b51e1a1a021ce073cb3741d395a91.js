b3894f5e124f7b61e3286498a2aad9a684ab39bb
ThatcheRRR <pavelosipov777@mail.ru>
2020-07-30 13:07:51 +0300

    Updates

---
+++ precache-manifest.c89b51e1a1a021ce073cb3741d395a91.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44e3fb49c75b29e60db2341eca397f9f",
+    "url": "/ThatcheRRR/todo-task/index.html"
+  },
+  {
+    "revision": "646dd594705b83acfb50",
+    "url": "/ThatcheRRR/todo-task/static/css/main.eb54dc22.chunk.css"
+  },
+  {
+    "revision": "cb2025b838cd9a1dbe91",
+    "url": "/ThatcheRRR/todo-task/static/js/2.f5dbd3df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ThatcheRRR/todo-task/static/js/2.f5dbd3df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "646dd594705b83acfb50",
+    "url": "/ThatcheRRR/todo-task/static/js/main.d3f943a8.chunk.js"
+  },
+  {
+    "revision": "d7174647671cbca79892",
+    "url": "/ThatcheRRR/todo-task/static/js/runtime-main.464fcae8.js"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/ThatcheRRR/todo-task/static/media/Roboto-Regular.11eabca2.ttf"
+  }
+]);
